86586c04a7b5ef5437022f3c9cf498d06b9fe3fc
nuraini604 <nuraiini604@gmail.com>
2025-04-30 23:03:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask, render_template
-import pymysql
+import mysql.connector
 
 app = Flask(__name__)
 
-db = pymysql.connect(
-    host='uts-database.cdkiiq0mub2o.ap-southeast-2.rds.amazonaws.com',
-    user='admin',
-    password='00000009', 
-    database='uts_db'
-)
+db_config = {
+    'host': 'uts-database.cdkiiq0mub2o.ap-southeast-2.rds.amazonaws.com',
+    'user': 'admin',
+    'password': '00000009',
+    'database': 'uts_db'
+}
 
 @app.route('/')
 def index():
-    cursor = db.cursor()
-    cursor.execute("SELECT id, nama_produk, harga, gambar_url FROM daftar_produk")
-    produk = cursor.fetchall()
-    return render_template("index.html", produk=produk)
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM daftar_produk")
+    products = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('index.html', products=products)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
